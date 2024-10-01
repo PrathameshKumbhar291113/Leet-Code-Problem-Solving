f9f0359e840533c3a5d68535c0e9e044f164f5ad
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2024-10-01 19:53:55 +0530

    [Working on problem]:  Remove Duplicates from Sorted Array.kt

--- src/EASY - Length Of Last Word.kt
+++ src/EASY - Length Of Last Word.kt
@@ -1,7 +1,7 @@
 
 fun main() {
 
-    //easy level question : Length Of Last Word
+    //EASY : Length Of Last Word
 
     var s = "   Hello   Worldddddd   "
     var words = s.reversed().trim().split(" ").filter { it.isNotBlank() }[0].length
